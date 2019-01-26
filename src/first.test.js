d732e6ea001540e078e9dc79045b2d10df234211
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-01-26 23:45:57 +0900

    add

---
+++ src/first.test.js
@@ -0,0 +1,5 @@
+import { sum } from "./first";
+
+it("", () => {
+  expect(sum(1,2)).toBe(3)
+})
--- src/first.test.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from "./first";
-
-// @ts-ignore
-describe("", () => {
-  // @ts-ignore
-  expect(sum(1,2)).toEqaul(3)
-})
